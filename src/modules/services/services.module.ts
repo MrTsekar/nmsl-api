41bfaa3f587c11a27470caa5b8ae92d85cbd9522
mrtsekar <mr.tsekar01@gmail.com>
2026-05-01 14:44:37 +0100

    Add public services endpoint for frontend consumption

--- src/modules/services/services.module.ts
+++ src/modules/services/services.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Service } from './entities/service.entity';
 import { ServicesService } from './services/services.service';
 import { ServicesController } from './controllers/services.controller';
+import { PublicServicesController } from './controllers/public-services.controller';
 import { FileUploadModule } from '../file-upload/file-upload.module';
 
 @Module({
@@ -10,7 +11,7 @@ import { FileUploadModule } from '../file-upload/file-upload.module';
     TypeOrmModule.forFeature([Service]),
     FileUploadModule,
   ],
-  controllers: [ServicesController],
+  controllers: [ServicesController, PublicServicesController],
   providers: [ServicesService],
   exports: [ServicesService],
 })
